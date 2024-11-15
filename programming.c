1a3c395bfa94b7deb9a5c6ed8152e7b713f48de7
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-15 14:49:24 +0600

    Implementing Frequency array

--- programming.c
+++ programming.c
@@ -1,10 +1,66 @@
+// #include<stdio.h>
+// int main(){
+
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n + 5];
+
+//     for(int i = 0; i < n; i++){
+//         scanf("%d", &a[i]);
+//     }
+
+//     int f[100000] = {0};
+
+//     for(int i = 0; i < n; i++){
+//         f[a[i]] = 1;
+//     }
+
+//     for(int i = 0; i < 10; i++){
+//         printf("%d %d\n", i , f[i]);
+//     }
+
+//     printf("%d", f[4]);
+
+//     return 0;
+// }
 #include<stdio.h>
 int main(){
 
-    int a,b;
-    scanf("%d %d", &a,&b);
+    int n;
+    scanf("%d", &n);
 
-    printf("%d %d", a ,b );
+    int a[n + 5];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    int f[100000] = {0};
+
+    for(int i = 0; i < n; i++){
+        f[a[i]] = 1;
+    }
+
+    for(int i = 0; i < 10; i++){
+        printf("%d %d\n", i , f[i]);
+    }
+
+    int m;
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++){
+        int x;
+        scanf("%d", &x);
+
+        printf("%d", x);
+
+        if(f[x] == 1){
+            printf(" ase\n");
+        }else{
+            printf(" nai\n");
+        }
+    }
 
     return 0;
 }
