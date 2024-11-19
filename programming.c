8b6e51cfe219199f68267bb810b032f7d5a8f068
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-19 22:41:53 +0600

    practise string

--- programming.c
+++ programming.c
@@ -24,43 +24,68 @@
 
 //     return 0;
 // }
-#include<stdio.h>
-int main(){
+// #include<stdio.h>
+// int main(){
 
-    int n;
-    scanf("%d", &n);
+//     int n;
+//     scanf("%d", &n);
 
-    int a[n + 5];
+//     int a[n + 5];
 
-    for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
-    }
+//     for(int i = 0; i < n; i++){
+//         scanf("%d", &a[i]);
+//     }
 
-    int f[100000] = {0};
+//     int f[100000] = {0};
 
-    for(int i = 0; i < n; i++){
-        f[a[i]] = 1;
-    }
+//     for(int i = 0; i < n; i++){
+//         f[a[i]] = 1;
+//     }
 
-    for(int i = 0; i < 10; i++){
-        printf("%d %d\n", i , f[i]);
-    }
+//     for(int i = 0; i < 10; i++){
+//         printf("%d %d\n", i , f[i]);
+//     }
 
-    int m;
-    scanf("%d", &m);
+//     int m;
+//     scanf("%d", &m);
 
-    for(int i = 0; i < m; i++){
-        int x;
-        scanf("%d", &x);
+//     for(int i = 0; i < m; i++){
+//         int x;
+//         scanf("%d", &x);
 
-        printf("%d", x);
+//         printf("%d", x);
 
-        if(f[x] == 1){
-            printf(" ase\n");
-        }else{
-            printf(" nai\n");
-        }
-    }
+//         if(f[x] == 1){
+//             printf(" ase\n");
+//         }else{
+//             printf(" nai\n");
+//         }
+//     }
 
+//     return 0;
+// }
+
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//      char a[100],b[100];
+//      scanf("%s %s",&a ,&b);
+//      for(int i=0;i<=strlen(b);i++)
+// {
+//     a[i] = b[i];
+// }
+// printf("%s %s",a,b);
+// return 0;
+// }
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", &a, &b);
+    strcpy(a, b);
+    printf("%s %s", a, b);
     return 0;
 }
